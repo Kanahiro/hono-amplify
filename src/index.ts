35720290a33f8d66bad7c8175750edce210ce0df
Kanahiro <mediterranean1769@gmail.com>
2025-02-25 09:48:35 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+	return c.text('Hello Hono!');
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get('/random', (c) => {
+	return c.json({ random: Math.random() });
+});
+
+let count = 0;
+app.get('/count', (c) => {
+	return c.json({ count: count++ });
+});
+
+serve(
+	{
+		fetch: app.fetch,
+		port: 3000,
+	},
+	(info) => {
+		console.log(`Server is running on http://localhost:${info.port}`);
+	},
+);
